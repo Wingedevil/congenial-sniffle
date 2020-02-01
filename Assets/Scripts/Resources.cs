e3f5884359249a99e80982f29edbc1d018db8ffe
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2020-02-01 09:47:36 +0800

    WIP stuff

--- Assets/Scripts/Resources.cs
+++ Assets/Scripts/Resources.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct Resources
+public class Resources
 {
     public int Wood;
     public int Steel;
     public int Gold;
     public int Happiness;
     public int Population;
-    public int RiverSize;
+    public int RiverSize = 3;
     public int Storage;
     public int Actions;
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : Manager<UIManager>
+{
+    public GameObject CardPrefab;
+    public Transform RiverStart;
+    public Transform StorageStart;
+
+    public List<GameObject> RiverObjects;
+    public List<GameObject> AssetObjects;
+
+    public enum ClickType
+    {
+        REPAIR,
+        SCRAP
+    }
+    public ClickType CurrentClickType = ClickType.REPAIR;
+    public void DrawRiver()
+    {
+        foreach (GameObject go in RiverObjects)
+        {
+            Destroy(go);
+        }
+        RiverObjects.Clear();
+
+        int i = 0;
+        foreach (Card card in GameLogicManager.Instance.PlayerRiver)
+        {
+            if (card == null)
+            {
+                ++i;
+                continue;
+            }
+            GameObject go = GameObject.Instantiate(CardPrefab, RiverStart.position + Vector3.right * 1.3f * i, Quaternion.identity, transform);
+            RiverObjects.Add(go);
+            UICard uiCard = go.GetComponent<UICard>();
+            uiCard.CardData = card;
+            uiCard.UpdateCardData();
+            ++i;
+        }
+    }
+
+    public void DrawAssets()
+    {
+        foreach (GameObject go in AssetObjects)
+        {
+            Destroy(go);
+        }
+        AssetObjects.Clear();
+
+        int i = 0;
+        foreach (Card card in GameLogicManager.Instance.PlayerAssets)
+        {
+            if (card == null)
+            {
+                ++i;
+                continue;
+            }
+            GameObject go = GameObject.Instantiate(CardPrefab, StorageStart.position + Vector3.right * 1.3f * i, Quaternion.identity, transform);
+            AssetObjects.Add(go);
+            UICard uiCard = go.GetComponent<UICard>();
+            uiCard.CardData = card;
+            uiCard.UpdateCardData();
+            uiCard.enabled = false;
+            ++i;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            GameLoopManager.Instance.NoMoreActions();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (CurrentClickType == ClickType.REPAIR)
+            {
+                CurrentClickType = ClickType.SCRAP;
+            }
+            else
+            {
+                CurrentClickType = ClickType.REPAIR;
+            }
+            Debug.Log(CurrentClickType);
+        }
+    }
+}
