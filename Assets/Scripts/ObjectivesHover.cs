e570d36b08dd2a8ee7064bd50f2f0f6c244374d5
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2020-02-02 13:39:24 +0800

    upkeep

--- Assets/Scripts/ObjectivesHover.cs
+++ Assets/Scripts/ObjectivesHover.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
+using TMPro;
 public class ObjectivesHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-
+    public TextMeshProUGUI UpkeepText;
     public GameObject Objectives;
     public void OnPointerEnter(PointerEventData e)
     {
+        int Upkeep = Mathf.FloorToInt(GameLogicManager.HAPPINESS_PER_POPULATION * GameLogicManager.Instance.PlayerResources.Population * -1);
+        UpkeepText.text = string.Format("Upkeep: {0} per turn", Upkeep);
         Objectives.SetActive(true);
     }
 
