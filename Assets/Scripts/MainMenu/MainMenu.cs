e88079f7014cd47ac76b4bf284c049c1c8684791
Wingedevil <jerroldtan@gmail.com>
2020-02-02 11:07:35 +0800

    Added Main Menu?

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadScene(string str) {
+        SceneManager.LoadScene(str);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/Resources.cs
+++ Assets/Scripts/Resources.cs
@@ -8,7 +8,7 @@ public class Resources
     public int Steel = 1;
     public int Gold = 1;
     public int Happiness = 100;
-    public int Population = 10;
+    public int Population = 5;
     public int PopulationAtWork = 2;
     public int RiverSize = 3;
     public int Storage = 5;
